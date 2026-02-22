4c7c4b703591cd97fd461e470b811787c3615268
Stefan le Roux <stefan@vivtec.co.za>
2026-02-22 20:37:08 +0200

    Astro Image component implementation

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,19 +4,16 @@ import { z } from "astro/zod";
 
 const blogs = defineCollection({
 	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/archives/blogs" }),
-	schema: z.object({
-		title: z.string(),
-		pubDate: z.date(),
-		description: z.string(),
-		author: z.string(),
-		image: z
-			.object({
-				url: z.string(),
-				alt: z.string(),
-			})
-			.optional(),
-		tags: z.array(z.string()),
-	}),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			pubDate: z.date(),
+			description: z.string(),
+			author: z.string(),
+			image: image(),
+			alt: z.string(),
+			tags: z.array(z.string()),
+		}),
 });
 
 export const collections = { blogs };
